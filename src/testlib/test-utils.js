253f8e7ebb9aef032e601529b11f37033bf48451
iankhor <iankhorbc.thecoder@gmail.com>
2020-02-15 15:55:16 +1100

    ignore test-utils files in jest

--- src/testlib/test-utils.js
+++ src/testlib/test-utils.js
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 import React from 'react';
 import { render } from '@testing-library/react';
 
@@ -9,4 +11,14 @@ const TestHook = ({ callback }) => {
 export const testHook = (callback, testSetup = () => {}) => {
   testSetup();
   render(<TestHook callback={callback} />);
+};
+
+export const mockPostAxios = ({ mockAxios, mockUrl, successResponse = {}, failResponse = {} }) => {
+  mockAxios.post.mockImplementationOnce(url => {
+    if (url === mockUrl) {
+      Object.keys(successResponse).length ? Promise.resolve(successResponse) : Promise.resolve(failResponse);
+    } else {
+      return Promise.reject({ status: 400 });
+    }
+  });
 };

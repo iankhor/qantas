a60c24aa645debbb583e84c939cf62b5c670724d
iankhor <iankhorbc.thecoder@gmail.com>
2020-02-15 16:05:19 +1100

    add data fixtures

---
+++ src/testlib/fixtures.js
@@ -0,0 +1,35 @@
+/* istanbul ignore file */
+
+export const airportDetails = [
+  {
+    airportCode: 'AAA',
+    internationalAirport: false,
+    domesticAirport: false,
+    regionalAirport: false,
+    onlineIndicator: false,
+    eticketableAirport: false,
+    location: {
+      aboveSeaLevel: -99999,
+      latitude: 17.25,
+      latitudeRadius: -0.304,
+      longitude: 145.3,
+      longitudeRadius: -2.5395,
+      latitudeDirection: 'S',
+      longitudeDirection: 'W'
+    },
+    airportName: 'Anaa',
+    city: {
+      cityCode: 'AAA',
+      cityName: 'Anaa',
+      timeZoneName: 'Pacific/Tahiti'
+    },
+    country: {
+      countryCode: 'PF',
+      countryName: 'French Polynesia'
+    },
+    region: {
+      regionCode: 'SP',
+      regionName: 'South Pacific'
+    }
+  }
+];

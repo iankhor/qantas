207dd98398f1e8ef763fdd77ea1e03ea86655e81
iankhor <iankhorbc.thecoder@gmail.com>
2020-02-16 22:55:43 +1100

    add total pages

--- src/hooks/usePaginator.jsx
+++ src/hooks/usePaginator.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 
-const PAGE_LIMIT = 10;
+const PAGE_LIMIT = 5;
 
 const usePaginator = () => {
   const [paginatedList, setPaginatedList] = useState([]);
+  const [totalPages, setTotalPages] = useState([]);
 
   const paginate = ({ list, pageNumber }) => {
     const tempPageNumber = pageNumber || 1;
     const firstIndex = (tempPageNumber - 1) * PAGE_LIMIT;
     const lastIndex = tempPageNumber * PAGE_LIMIT;
 
+    setTotalPages(Math.ceil(list.length / PAGE_LIMIT));
     setPaginatedList(list.slice(firstIndex, lastIndex));
   };
 
-  return [{ paginatedList }, paginate];
+  return [{ paginatedList, totalPages }, paginate];
 };
 
 export default usePaginator;
--- src/hooks/usePaginator.test.jsx
+++ src/hooks/usePaginator.test.jsx
@@ -5,38 +5,46 @@ import { act } from 'react-dom/test-utils';
 describe('usePaginator', () => {
   let hooksOpts;
 
-  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
   beforeEach(() => {
     testHook(() => {
       hooksOpts = usePaginator([]);
     });
   });
 
-  it('returns a list of 10 items by default for the first page', () => {
+  it('returns a list of 5 items by default for the first page', () => {
     const [_, paginate] = hooksOpts;
     act(() => paginate({ list, pageNumber: 1 }));
     const [{ paginatedList }] = hooksOpts;
 
-    expect(paginatedList).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+    expect(paginatedList).toEqual(expect.arrayContaining([1, 2, 3, 4, 5]));
+  });
+
+  it('returns the total pages', () => {
+    const [_, paginate] = hooksOpts;
+    act(() => paginate({ list }));
+    const [{ totalPages }] = hooksOpts;
+
+    expect(totalPages).toEqual(3);
   });
 
   describe('if page number not supplied', () => {
-    it('returns a list of 10 items by default for the first page', () => {
+    it('returns a list of 5 items by default for the first page', () => {
       const [_, paginate] = hooksOpts;
       act(() => paginate({ list }));
       const [{ paginatedList }] = hooksOpts;
 
-      expect(paginatedList).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+      expect(paginatedList).toEqual(expect.arrayContaining([1, 2, 3, 4, 5]));
     });
   });
 
   describe('the second page', () => {
-    it('returns the next 10 items in the list', () => {
+    it('returns the next 5 items in the list', () => {
       const [_, paginate] = hooksOpts;
       act(() => paginate({ list, pageNumber: 2 }));
       const [{ paginatedList }] = hooksOpts;
 
-      expect(paginatedList).toEqual([11, 12, 13, 14, 15]);
+      expect(paginatedList).toEqual([6, 7, 8, 9, 10]);
     });
   });
 });
